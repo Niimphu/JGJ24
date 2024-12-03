a522084257f263fabc3ed9177fad3bcc2922fc40
kiabdura <kimjanabdurahman2002@outlook.de>
2024-12-03 18:40:09 +0100

    "merged" branches
    -renamed dash to more apt roll
    -reattached scrips
    -added comments regarding major issues in player script regarding delta, roll speed and original roll_vector. will fix next time

--- scenes/game/Player/player.gd
+++ scenes/game/Player/player.gd
@@ -2,43 +2,47 @@ extends CharacterBody2D
 
 enum {
 	MOVE,
-	DASH
+	ROLL
 }
 
-@onready var bullet_scene = preload("res://Player/bullet.tscn")
+@onready var bullet_scene = preload("bullet.tscn")
 const SPEED = 700
 const FRICTION = 800
 var state = MOVE
-var dash_vector = Vector2.LEFT
+
+#still assumes roll to the left without prior input, needs fixing
+var roll_vector = Vector2.LEFT
 
 
 func _physics_process(delta: float) -> void:
 	match state:
 		MOVE:
-			move_state(delta)
-		DASH:
-			dash_state(delta)
+			move_state()
+		ROLL:
+			roll_state()
 	if Input.is_action_just_pressed("fire"):
 		shoot()
 
-func move_state(delta):
+#needs delta so speed isn't tied to FPS
+func move_state():
 	var input_vector = Vector2.ZERO
 
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
-		dash_vector = input_vector
+		roll_vector = input_vector
 		velocity = input_vector * SPEED
 	else:
 		velocity = velocity.move_toward(input_vector, FRICTION)
 	move_and_slide()
-	if Input.is_action_just_pressed("dash"):
-		state = DASH
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 
-#this shortens the dash when 
-func dash_state(delta):
-	velocity = dash_vector * 5000
+#this shortens the roll while moving, needs fixing
+#needs delta so speed isn't tied to fps
+func roll_state():
+	velocity = roll_vector * 5000
 	move_and_slide()
 	roll_animation_finished()
 
